c4bf3435fb016924e3f2cb1808a52a24afe39407
Ricardo Augusto Kowalski <ricardoakowalski@gmail.com>
2026-08-27 22:55:17 -0300

    feat: implement Checkout component and integrate it into Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+import { Checkout } from '@/features/checkout/components/checkout'
+import { getCheckoutProduct } from '@/features/checkout/services/get-checkout-product'
+
 export default function Home() {
-	return null
+	const product = getCheckoutProduct()
+
+	return <Checkout product={product} />
 }
